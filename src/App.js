35045b52979fbfbfb1c9ff2bf1d02308d80ed541
saifulemon <463saiful0@gmail.com>
2022-06-16 22:47:10 +0600

    routing started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/review' element={<OrderReview />} />
+          <Route path='/inventory' element={<Inventory />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{textAlign: 'center', color: 'red'}}>
+            <h1>404</h1>
+            <p>Page Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
