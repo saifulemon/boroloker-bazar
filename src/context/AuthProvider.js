dd8fbb1e912e05ac2fe45bedb12ef46c227bb0b0
saifulemon <463saiful0@gmail.com>
2022-07-20 15:10:05 +0600

    Create user context and set logged in user in context

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
